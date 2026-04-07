1d3a4dc8f1af5cba6e3a829e441277dd07de5309
Daniel25VF <danielvergarafondevila@gmail.com>
2026-04-07 15:44:24 +0200

    Delete web directory

--- web/src/config/cognito.ts
+++
@@ -1,5 +0,0 @@
-export const cognitoConfig = {
-  region: import.meta.env.VITE_AWS_REGION,
-  userPoolId: import.meta.env.VITE_USER_POOL_ID,
-  userPoolWebClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
-};
